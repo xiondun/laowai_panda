d24acbd9cc9d6c56c2d1f3ef6f41a64209ae6887
xd <893287656@qq.com>
2020-07-23 15:32:20 +0800

    1

--- get_remote_img_files.py
+++ get_remote_img_files.py
@@ -1,12 +1,15 @@
 import datetime
 import os
 import time
+import traceback
 from concurrent.futures.thread import ThreadPoolExecutor
 
 import pymysql
 import requests
 
 conn = pymysql.connect("localhost", "root", "PandaPass!2", "laowai_panda_db_laowai_panda")
+
+
 def get_unsynchronized_imgs():
     """
     获取未同步的图片
@@ -51,21 +54,27 @@ def mkdir(path):
     else:
         return False
 
+
 save_path = '/var/www/api/laowai_panda/media/'
 
-def down_load_img(imgUrl, imgName,id):
+
+def down_load_img(imgUrl, imgName, id):
+    save_img_path = save_path + str(imgName)
+    if os.path.exists(save_img_path):
+        print('图片已经存在：', imgName)
+        update_unsynchronized_imgs(id)
+        return
     img_path = imgName.split('/')
     if len(img_path) > 1:
         save_dir = save_path + img_path[0] + '/'
         mkdir(save_dir)
-    save_img_path = save_path + str(imgName)
     try:
         header = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
         }
         res = requests.get(imgUrl, headers=header, timeout=120)
         if res.status_code != 200:
-            print(imgUrl,'下载网络错误：',res.status_code)
+            print(imgUrl, '下载网络错误：', res.status_code)
             return False
         with open(save_img_path, 'wb') as f:
             f.write(res.content)
@@ -73,6 +82,7 @@ def down_load_img(imgUrl, imgName,id):
         update_unsynchronized_imgs(id)
         return save_img_path
     except Exception as e:
+        traceback.print_exc()
         print(imgUrl, "下载图片错误XXXX", e)
         try:
             os.remove(save_img_path)
@@ -81,21 +91,23 @@ def down_load_img(imgUrl, imgName,id):
         return False
 
 
-def do_down_load_remote_img(imgName,id):
+def do_down_load_remote_img(imgName, id):
     downloadImgUrlList = [
         'http://45.13.199.57/media/' + imgName,  # 德國
         'http://121.40.208.210/media/' + imgName,  # 杭州
     ]
     for imgUrl in downloadImgUrlList:
-        down_load_img(imgUrl, imgName,id)
+        down_load_img(imgUrl, imgName, id)
+
 
 def main():
     images = get_unsynchronized_imgs()
     with ThreadPoolExecutor(max_workers=30) as thread:
         for image in images:
             # do_down_load_remote_img(imgName)
-            thread.submit(do_down_load_remote_img, image['imgName'],image['id'])
+            thread.submit(do_down_load_remote_img, image['imgName'], image['id'])
+
 
 if __name__ == '__main__':
     main()
-    print('完成',datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print('完成', datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
