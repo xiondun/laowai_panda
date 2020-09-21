8d52c2e85f3808b1988de60f61d4148054cc9288
xd <893287656@qq.com>
2020-09-21 10:21:53 +0800

    1

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -149,8 +149,8 @@ class GetProfileSerializer(serializers.ModelSerializer):
     default_lang = DefaultLanguageSerializer(read_only=True)
     followers_count = serializers.SerializerMethodField()
     following_count = serializers.SerializerMethodField()
-    likes = serializers.SerializerMethodField()
-    usefuls = serializers.SerializerMethodField()
+    # likes = serializers.SerializerMethodField()
+    # usefuls = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -161,11 +161,11 @@ class GetProfileSerializer(serializers.ModelSerializer):
             'email': {'read_only': True},
         }
 
-    def get_likes(self, obj):
-        return 3
-
-    def get_usefuls(self, obj):
-        return obj.usefuls
+    # def get_likes(self, obj):
+    #     return obj.likes
+    #
+    # def get_usefuls(self, obj):
+    #     return obj.usefuls
 
     def get_followers_count(self, obj):
         return obj.followers_count
